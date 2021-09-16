c95b194ea6f025104fbfe9193a144361a0d9fe05
GuilhermeSR98 <guigareisbr@gmail.com>
2021-09-16 12:01:50 -0300

    :sparkles: Creating routing and navigation between Home and New Room

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/room/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,6 @@
+import { Link } from "react-router-dom";
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
 export function NewRoom() {
@@ -15,16 +15,13 @@ export function NewRoom() {
         <div className="main-content">
           <img src={logoImg} alt="Asking" />
           <h2>Criar uma nova sala</h2>
-          <button className="create-room">
-            <img src={googleIconImg} alt="Logo do Google" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator">Ou entre em uma sala</div>
           <form>
             <input placeholder="Nome da Sala" type="text" />
             <Button type="submit">Criar sala</Button>
           </form>
-          <p>Quer entar em uma sala existente? <a href="#">Clique aqui</a> </p>
+          <p>
+            Quer entar em uma sala existente? <Link to="/">Clique aqui</Link>{" "}
+          </p>
         </div>
       </main>
     </div>
